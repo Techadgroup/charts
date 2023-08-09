1d668e85270bb88d31faa2894ab99b0278e203c9
VanessaAdjei <74023710+VanessaAdjei@users.noreply.github.com>
2023-08-09 15:34:42 +0000

    Add files via upload

---
+++ src/app/mixed-chart.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mixed-chart',
+  templateUrl: './mixed-chart.component.html',
+  styleUrls: ['./mixed-chart.component.css']
+})
+export class MixedChartComponent {
+
+}
