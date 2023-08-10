fd289751d285b6735ce785d14c216349a870ae4c
VanessaAdjei <74023710+VanessaAdjei@users.noreply.github.com>
2023-08-10 10:14:39 +0000

    Update mixed-chart.component.ts

--- src/app/mixed-chart.component.ts
+++ src/app/mixed-chart.component.ts
@@ -12,7 +12,17 @@ export class MixedChartComponent {
     { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B', type: 'bar' },
     { data: [180, 480, 770, 90, 1000, 270, 400], label: 'Series C', type: 'bar' },
     
+
+    {
+      data: [], 
+      label: 'Curve',
+      type: 'line',
+      fill: false,
+    },
+
   ];
+  
+  
 
   mixedChartLabels: string[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
 
@@ -23,26 +33,38 @@ export class MixedChartComponent {
         type: 'category',
         labels: this.mixedChartLabels 
       },
+     
       y: {
-        beginAtZero: true, 
+        beginAtZero: true,
         title: {
           display: true,
           text: 'Value'
         }
-      },
-    
+      }
     }
   };
 
+mixedChartType: any;
 
 
  
   constructor() {
    
+    this.calculateLineGraphData();
    }
 
 
    
+
+  private calculateLineGraphData(): void {
+    const seriesAData = this.mixedChartData[0].data as number[];
+    const seriesBData = this.mixedChartData[1].data as number[];
+    const seriesCData = this.mixedChartData[2].data as number[];
+
+    const curveData = seriesAData.map((value, index) => value + seriesBData[index] + seriesCData[index]);
+    this.mixedChartData[3].data = curveData;
+  }
   ngOnInit(): void {
+    
   }
 }
